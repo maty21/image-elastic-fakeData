ca2afe5c68ebb21215a2e708977cd4f61459daa8
maty zisserman <maty21@gmail.com>
2018-06-11 09:44:16 -1000

    reinit data with faker and add location ad geo point

--- app.js
+++ app.js
@@ -17,34 +17,64 @@ const generateDataForIndex = () => {
     generateData('resolutionWidth', [1024, 2048, 768])
     generateData('resolutionHeight', [248, 12, 768])
 }
-const fakedata = (amount = 100) => {
-    Array(amount).fill().forEach(async n => {
-        try {
-           let data =  await client.index({
-                index: 'image',
-                type: 'imageType',
-                body: {
-                    sensor: getRandomByType('sensor'),
-                    location: getRandomByType('location'),
-                    image: faker.image.avatar(),
-                    date: faker.date.past(),
-                    sensorKind: getRandomByType('sensorKind'),
-                    resolutionWidth: getRandomByType('resolutionWidth'),
-                    resolutionHeight: getRandomByType('resolutionHeight'),
-                    point:getRandomPoint()
-                    
-
-                }
-            })
-        } catch (error) {
-            console.log(error);
-        }
-       
+const fakedata = async (amount = 100) => {
+ if(await client.indices.exists({index:'image'})){
+   await client.indices.delete({
+        index:'image'
     })
+ }
+ await client.indices.create({
+    index:'image'
+})
+await mapping();
+ Array(amount).fill().forEach(async n => {
+    try {
+       let data =  await client.index({
+            index: 'image',
+            type: 'imageType',
+            body: {
+                sensor: getRandomByType('sensor'),
+                location: getRandomByType('location'),
+                image: faker.image.avatar(),
+                date: faker.date.past(),
+                sensorKind: getRandomByType('sensorKind'),
+                resolutionWidth: getRandomByType('resolutionWidth'),
+                resolutionHeight: getRandomByType('resolutionHeight'),
+                point:getRandomPoint()
+                
 
+            }
+        })
+       
+    } catch (error) {
+        console.log(error);
+    }
+   
+})
 
 }
 
+
+const mapping =async ()=>{
+    try {
+     
+       await client.indices.putMapping({
+            index: 'image',
+            type: 'imageType',
+            body: {
+                properties: {
+                    point: {
+                      type: "geo_point"
+                    }
+                   
+            }  
+        }
+        })   
+        
+    } catch (e) {
+        console.log(e);
+    } 
+}
 const generateData = (type, arr) => data[type] = { data: arr, size: arr.length }
 const generateDataFaker = (fakerType, size, fakerFunction) => {
     data[fakerType] = {
@@ -58,7 +88,7 @@ const generateDataFaker = (fakerType, size, fakerFunction) => {
 }
 
 const getRandomByType = (fakerType) => data[fakerType].data[Math.round(Math.random() * data[fakerType].size - 1)]
-const getRandomPoint= ()=> ({latitude: getPointHelper("39.83"),longitude: getPointHelper("-74.87")}) 
+const getRandomPoint= ()=> ({lat: getPointHelper("39.83"),lon: getPointHelper("-74.87")}) 
 //cli
 
 const getPointHelper = (point)=> point+Math.floor(Math.random() * 10);
